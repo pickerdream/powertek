01b8aa342f4324f402d6fdff4a76c5efa73ae529
mixcode <59798480+submix2133@users.noreply.github.com>
2022-05-29 13:20:47 +0900

    サイト公開

---
+++ main.js
@@ -0,0 +1,19 @@
+$(function() {
+    $('.hamburger').click(function() {
+        $(this).toggleClass('active');
+ 
+        if ($(this).hasClass('active')) {
+            $('.globalMenuSp').addClass('active');
+        } else {
+            $('.globalMenuSp').removeClass('active');
+        } 
+      
+    });
+});
+$(function() {
+    $('.globalMenuSp a[href]').click(function() {
+        $('.globalMenuSp').removeClass('active');
+       $('.hamburger').removeClass('active');
+
+    });
+});
